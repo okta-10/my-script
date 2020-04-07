4e6ae87cce4046c024cef6d713ff61ab6b037f6e
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-04-07 13:50:51 +0700

    whyred: update script for whyred
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- whyred.sh
+++ whyred.sh
@@ -8,34 +8,108 @@ echo -e "$yellow"
 
 # environtment
 KERNEL_DIR=$PWD
-KERNEL_OUT=$pwd/out/arch/arm64/boot/Image.gz-dtb
-CONFIG=$pwd/mystic-whyred_defconfig
+KERNEL_OUT=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+AK3_DIR=$KERNEL_DIR/../AnyKernel3
+CONFIG=mystic-whyred_defconfig
 CORE="$(grep -c '^processor' /proc/cpuinfo)"
-CCACHE="$(command -v ccache)"
 
-# Toolchain setup
-export ARCH="arm64"
-CROSS_COMPILE="$PWD/../Toolchains/aarch64-49/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32="$PWD/../Toolchains/arm-49/bin/arm-linux-androideabi-"
-
-# Clang setup
-CLANG_DIR="$PWD/../Toolchains/clang10"
-CLANG_TRIPLE="aarch64-linux-gnu-"
-CC="$CLANG_DIR/bin/clang"
-KBUILD_COMPILER_STRING=$(${CC} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+# Setup GCC Toolchain
+CROSS_COMPILE="$PWD/../Toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32="$PWD/../Toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
 
 # Export
-export CCACHE KBUILD_COMPILER_STRING CLANG_TRIPLE CORE CROSS_COMPILE CROSS_COMPILE_ARM32
+export TZ=":Asia/Jakarta"
+export ARCH=arm64
+export SUBARCH=arm64
+export CROSS_COMPILE
+export CROSS_COMPILE_ARM32
 
-start () {
-        time make -j"${CORE}" \
-            O=out \
-            ARCH="${ARCH}" \
-            CC="${CCACHE} ${CC}" \
-            CLANG_TRIPLE="${CLANG_TRIPLE}" \
-            CROSS_COMPILE="${CROSS_COMPILE}" \
-            CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"
-}
+while true; do
+    # Menu
+	echo -e ""
+    echo -e " Menu                                                               "
+    echo -e " ╔═════════════════════════════════════════════════════════════════╗"
+    echo -e " ║ 1. Export Whyred defconfig to Out Dir                           ║"
+    echo -e " ║ 2. Start Compile GCC Only                                       ║"
+    echo -e " ║ 3. Start Compile With Clang                                     ║"
+    echo -e " ║ 4. Copy Image.gz-dtb to Flashable Dir                           ║"
+    echo -e " ║ e. Back Main Menu                                               ║"
+    echo -e " ╚═════════════════════════════════════════════════════════════════╝"
+    echo -ne "\n Enter your choice 1-4, or press 'e' for back to Main Menu : "
 
-make O=out ARCH=arm64 $CONFIG
-start
+    read menu
+
+    # Export deconfig
+    if [[ "$menu" == "1" ]]; then
+        make O=out $CONFIG
+        echo -e "\n (i) Success export $CONFIG defonfig to Out Dir"
+        echo -e ""
+    fi
+
+    # build GCC only
+    if [[ "$menu" == "2" ]]; then
+        start () {
+                time make -j"${CORE}" \
+                O=out \
+                ARCH="${ARCH}" \
+                CROSS_COMPILE="${CROSS_COMPILE}" \
+                CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"
+            }
+
+        START=$(date +"%s")
+        CURRENTDATE=`date +"%A, %d %b %Y, %H:%M:%S"`
+        echo -e "\n (i) Compile kernel started at $CURRENTDATE using $CORE thread"
+        echo -e "\n"
+        make O=out
+        END=$(date +"%s")
+        TOTAL_TIME=$(($END - $START))
+        echo -e "\n (i) Compile successfully, Kernel Image in $KERNEL_OUT"
+        echo -e " (i) Total time elapsed: $(($TOTAL_TIME / 60)) Minutes, $(($TOTAL_TIME % 60)) Second."
+        echo -e "\n"
+     fi
+
+    # Build With Clang
+    if [[ "$menu" == "3" ]]; then
+
+        # Setup Clang
+        CLANG_DIR="$PWD/../Toolchain/clang-r377782c"
+        export CLANG_TRIPLE="aarch64-linux-gnu-"
+        export CC="$CLANG_DIR/bin/clang"
+        export KBUILD_COMPILER_STRING=$(${CC} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+
+        start () {
+                time make -j"${CORE}" \
+                O=out \
+                ARCH="${ARCH}" \
+                CC="${CC}" \
+                CLANG_TRIPLE="${CLANG_TRIPLE}" \
+                CROSS_COMPILE="${CROSS_COMPILE}" \
+                CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"
+            }
+
+        START=$(date +"%s")
+        CURRENTDATE=`date +"%A, %d %b %Y, %H:%M:%S"`
+        echo -e "\n (i) Compile kernel started at $CURRENTDATE using $CORE thread"
+        echo -e "\n"
+        make O=out
+        END=$(date +"%s")
+        TOTAL_TIME=$(($END - $START))
+        echo -e "\n (i) Compile successfully, Kernel Image in $KERNEL_OUT"
+        echo -e " (i) Total time elapsed: $(($TOTAL_TIME / 60)) Minutes, $(($TOTAL_TIME % 60)) Second."
+        echo -e "\n"
+    fi
+
+    # Move kernel to flashable dir
+    if [[ "$menu" == "4" ]]; then
+        cd $AK3_DIR
+        cp $KERNEL_OUT $AK3_DIR/Image.gz-dtb
+
+        echo -e "\n (i) Done moving kernel to $AK3_DIR"
+    fi
+
+    # Exit
+    if [[ "$menu" == "e" ]]; then
+    	exit
+    fi
+
+done
